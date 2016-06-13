13bda0ca254bf1a3cb702f211ef9d144a127cec0
Andrew Niekamp and Shokouh farvid <pair+andrew.f.niekamp+sherly789@gmail.com>
2016-06-13 16:09:55 -0700

    add sounds

---
+++ js/script.js
@@ -0,0 +1,11 @@
+  function ostrichsound(){
+    document.getElementById('ostrich').play();
+  }
+  //ostrichsound();
+  function monkeysound(){
+    document.getElementById('monkey').play();
+  }
+
+  function slothsound(){
+    document.getElementById('sloth').play();
+  }
